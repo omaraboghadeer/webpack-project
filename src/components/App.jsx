3db7f125d09377268fe1a50f3a2204324cf30ed6
omar.aboghadeer <omar.abo.ghadeer@hotmail.com>
2021-11-05 04:55:51 +0200

    add react support

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import "../styles/index.scss";
+import Recipes from "./Recipes";
+
+export default function App() {
+    return (
+        <>
+            <section className="here"></section>
+            <main>
+                <section>
+                    <h1>oh hai, React</h1>    
+                </section>    
+            </main>
+
+            <Recipes />
+        </>
+    )
+}
